40388a7cd5c15043ec93a3d9c2e18508151915d0
Da4ndo <contact@da4ndo.com>
2023-08-22 19:03:15 +0200

    up

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
-CREATE USER 'mailuser'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'adminMailuser@#$';
-GRANT ALL ON mailserver.* TO 'mailuser'@'localhost';
+CREATE USER 'mailuser'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'adminMailuser@#$';
+GRANT ALL ON mailserver.* TO 'mailuser'@'%';
 
 -- Remove test database and user
 DROP DATABASE IF EXISTS test;
