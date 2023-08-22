d35f9a58663d26daf06b9e707cffc9c52f5b2a9c
Da4ndo <contact@da4ndo.com>
2023-08-22 16:18:14 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals = config.externals || [];
+    config.externals.push(path.resolve(__dirname, 'src/api')); // Path to your Nest.js code
+
+    return config;
+  },
+};
+
